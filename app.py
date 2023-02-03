1d6c39f19fa0e2d2a337b24f8aa1143239baf986
Utkarsh <utkarshsteve7@gmail.com>
2023-02-03 16:36:02 +0530

    Adding error handling using abort from flask_smorest

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import uuid
 from flask import Flask, request
+from flask_smorest import abort
 from db import items, stores
 
 app = Flask(__name__)
@@ -18,6 +19,9 @@ def getAllItems():
 def addAStore():
     request_data = request.get_json()
     storeName = request_data.get('name', None)
+    for store in stores.values():
+        if storeName == store['name']:
+            abort(400, message=f'Store already exists')
     if storeName:
         print(f'Logging Store Name:{storeName}')
         store_id = uuid.uuid4().hex
@@ -25,7 +29,7 @@ def addAStore():
         stores[store_id] = new_store
         return new_store, 201
     else:
-        return {'message':f'Not able to create store as storeName:{storeName} is not valid'}
+        abort(400, message=f'Not able to create store as storeName:{storeName} is not valid')
     
 @app.post('/item')
 def addItemToExisitngStore():
@@ -34,16 +38,24 @@ def addItemToExisitngStore():
     price = request_data.get('price', None)
     store_id = request_data.get('store_id', None)
     payload_validation = item_name and price and store_id
-    if(payload_validation and store_id in stores):
+    if store_id not in stores:
+        abort(404, message=f'Store with store id:{store_id} not found')
+    
+    for item in items.values():
+        if (
+            item_name == item['name'] and store_id == item['store_id']
+        ):
+            abort(400, message=f'Item  with name:{item_name} and item_id:{item["id"]} already exists')
+        
+    if(payload_validation):
         item_id = uuid.uuid4().hex
         item = {**request_data, "id":item_id}
         items[item_id] = item
         return item, 201
     else:
-        return {'message':f'Either the item name:{item_name} or price:{price} is not valid or store with id:{store_id} does not exists'}, 404
-    
+        abort(400, message=f'Either the item name:{item_name} or price:{price} is not valid or store with id:{store_id} does not exists')
+        
 @app.get('/store/<string:store_id>/item')
-#TODO: improve this
 def getAllItemsFromAParticularStore(store_id):
     #use list comprehension
     filtered_items = {k:v for k,v in items.items() if v['store_id'] == str(store_id)}.values()
@@ -56,7 +68,7 @@ def getAllItemsFromAParticularStore(store_id):
                 }
             item.append(res)
         return item
-    return {'message': f'Store with store name:{store_id} not found'}, 404
+    abort(404, message=f'Store with store name:{store_id} not found')
     
 @app.get('/store/<string:store_id>')
 def getAParticularStore(store_id):
@@ -64,7 +76,7 @@ def getAParticularStore(store_id):
         return stores.get[store_id]
     except KeyError as e:
         print(f'Keyerror:{e}')
-        return {'message':f'Store not found'}, 404
+        abort(404, message=f'Store not found')
     
 @app.get('/item/<string:item_id>')
 def getAParticularItem(item_id):
@@ -72,4 +84,4 @@ def getAParticularItem(item_id):
         return items.get[item_id]
     except KeyError as e:
         print(f'Keyerror:{e}')
-        return {'message':f'Item not found'}, 404
+        abort(404, message=f'Item not found')
