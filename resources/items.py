c22f79bdb015d4beeb10ec49bd03fbc518adbdfe
Utkarsh <utkarshsteve7@gmail.com>
2023-05-01 14:46:19 +0530

    Using SQLAlchemy to perform crud operations

--- resources/items.py
+++ resources/items.py
@@ -2,9 +2,10 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from db import items, stores
 from schemas import ItemSchema, ItemUpdateSchema
-
+from models import ItemModel
+from db import db
+from sqlalchemy.exc import SQLAlchemyError
 
 blpItems = Blueprint("items", __name__, description="Operations on items")
 
@@ -13,76 +14,60 @@ blpItems = Blueprint("items", __name__, description="Operations on items")
 class ItemGetPutAndDelete(MethodView):
     @blpItems.response(200, ItemSchema)
     def get(self, item_id):
-        try:
-            return items.get(item_id)
-        except KeyError as e:
-            print(f'Keyerror:{e}')
-            abort(404, message=f'Item not found')
-            
+        item = ItemModel.query.get_or_404(
+            item_id)  # retrieve item by primary key
+        return item
+
     @blpItems.arguments(ItemUpdateSchema)
     @blpItems.response(200, ItemSchema)
     def put(self, request_data, item_id):
-        try:
-                item = items[item_id]
-                item |= request_data
-                return item
-        except KeyError as e:
-                print(f'KeyError:{e}')
-                abort(404, message=f"Item with item_id:{item_id} Not Found")
-            
+        item = ItemModel.query.get(item_id)  # retrieve item by primary key
+        if item:
+            item.price = request_data['price']
+            item.name = request_data['name']
+        else:
+            item = ItemModel(id=item_id, **request_data)
+
+        db.session.add(item)
+        db.session.commit(item)
+
+        return item
+
     def delete(self, item_id):
-        try:
-            del items[item_id]
-            return {'message': f'store with item_id :{item_id} deleted successfully'}
-        except KeyError as e:
-            print(f'Keyerror:{e}')
-            abort(404, message=f'Item with item_id:{item_id} not found')    
-            
+        item = ItemModel.query.get_or_404(
+            item_id)  # retrieve item by primary key
+        db.session.delete(item)
+        db.session.commit()
+        return {'message': f'Store with item id:{item_id} deleted'}
+
+
 @blpItems.route('/items')
 class ItemGetAllItems(MethodView):
     @blpItems.response(200, ItemSchema(many=True))
     def get(self):
-        return items.values()
-    
+        items = ItemModel.query.all()  # retrieve item by primary key
+        if items:
+            print(f'There are items in the db...')
+        else:
+            response = {
+                'message': 'There are no items in the db. Please try again...'}
+            return response
+        return items
+
+
 @blpItems.route('/item')
 class ItemPostCreateAStore(MethodView):
     @blpItems.arguments(ItemSchema)
     @blpItems.response(201, ItemSchema)
     def post(self, request_data):
-        item_name = request_data.get('name', None)
-        price = request_data.get('price', None)
-        store_id = request_data.get('store_id', None)
-        payload_validation = item_name and price and store_id
+        # unpacking dictionary into key and value pairs
+        item = ItemModel(**request_data)
+        print(
+            f'Logging the item when trying to psot an item to db...{request_data}')
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occured while inserting the item.")
 
-        for item in items.values():
-            if (
-                item_name == item['name'] and store_id == item['store_id']
-            ):
-                abort(
-                    400, message=f'Item  with name:{item_name} and item_id:{item["id"]} already exists')
-
-        if store_id not in stores:
-            abort(404, message=f'Store with store id:{store_id} not found')
-
-        if (payload_validation):
-            item_id = uuid.uuid4().hex
-            item = {**request_data, "id": item_id}
-            items[item_id] = item
-            return item
-        abort(
-            400, message=f'Either the item name:{item_name} or price:{price} is not valid or store with id:{store_id} does not exists')
-        
-@blpItems.route('/store/<string:store_id>/item')
-class ItemGetItemsFromAStore(MethodView):
-    def get(self, store_id):
-        filtered_items = {k: v for k, v in items.items() if v['store_id'] == str(store_id)}.values()
-        if filtered_items:
-            item = []
-            for i in range(len(filtered_items)):
-                res = {
-                    'name': list(filtered_items)[i]['name'],
-                    'price': list(filtered_items)[i]['price']
-                }
-                item.append(res)
-            return item
-        abort(404, message=f'Store with store name:{store_id} not found')
+        return item
--- resources/stores.py
+++ resources/stores.py
@@ -2,9 +2,10 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from db import stores
 from schemas import StoreSchema
-
+from models import StoreModel
+from db import db
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 
 blpStores = Blueprint("stores", __name__, description="Operations on stores")
 
@@ -13,42 +14,41 @@ blpStores = Blueprint("stores", __name__, description="Operations on stores")
 class StoreGetAndDelete(MethodView):
     @blpStores.response(200, StoreSchema)
     def get(self, store_id):
-        try:
-            return stores.get(store_id)
-        except KeyError as e:
-            print(f'Keyerror:{e}')
-            abort(404, message=f'Store not found')
+        store = StoreModel.query.get_or_404(store_id) # retrieve store by primary key
+        return store
 
     def delete(self, store_id):
-        try:
-            del stores[store_id]
-            return {'message': f'store with store id:{store_id} successfully'}
-        except KeyError as e:
-            print(f'Keyerror:{e}')
-            abort(404, message=f'Store with store id:{store_id} not found')
+        store = StoreModel.query.get_or_404(store_id) # retrieve store by primary key
+        db.session.delete(store)
+        db.session.commit()
+        return {'message': f'Store with store id:{store_id} deleted'}
             
 
 @blpStores.route('/stores')
 class StoreGetAllStores(MethodView):
     @blpStores.response(200, StoreSchema(many=True))
     def get(self):
-        return stores.values()
-    
+        stores = StoreModel.query.all() # retrieve item by primary key
+        if stores:
+            response = stores
+            print(f'There are stores in the db...')
+        else:
+            response = {'message': 'There are no stores in the db. Please try again...'}
+        return response
+        
     
 @blpStores.route('/store')
 class StoreCreateAnStore(MethodView):
     @blpStores.arguments(StoreSchema)
     @blpStores.response(201, StoreSchema)
     def post(self, request_data):
-        storeName = request_data.get('name', None)
-        for store in stores.values():
-            if storeName == store['name']:
-                abort(400, message=f'Store already exists')
-        if storeName:
-            print(f'Logging Store Name:{storeName}')
-            store_id = uuid.uuid4().hex
-            new_store = {**request_data, "id": store_id}
-            stores[store_id] = new_store
-            return new_store
-        abort(
-        400, message=f'Not able to create store as storeName:{storeName} is not valid')
+        store = StoreModel(**request_data)
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except IntegrityError:
+            abort(400, message="A store with that name already exists.")
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while creating the store")
+            
+        return store
